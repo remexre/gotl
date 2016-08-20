1e5f885ffbb688b37332305462db8511732d19f0
Nathaniel Ringo <remexre@gmail.com>
2016-08-19 21:40:41 -0500

    Improves test.

--- parser/parse_test.go
+++ parser/parse_test.go
@@ -25,19 +25,19 @@ four
 	six
 		seven`
 
-func TestParseProtonodes(t *testing.T) {
+func TestParseParts(t *testing.T) {
 	Convey("Valid input", t, func() {
-		protonodes := parseProtonodes("", testParseValid)
+		protonodes := parseProtonodes("testParseValid", testParseValid)
 		Convey("Should parse into protonodes", func() {
 			So(protonodes, ShouldResemble, []protonode{
-				{"", 0, 1, "doctype html"},
-				{"", 0, 2, "one"},
-				{"", 1, 3, "two"},
-				{"", 1, 4, "three"},
-				{"", 2, 5, "four"},
-				{"", 3, 6, "five"},
-				{"", 2, 7, "six"},
-				{"", 2, 8, "seven"},
+				{"testParseValid", 0, 1, "doctype html"},
+				{"testParseValid", 0, 2, "one"},
+				{"testParseValid", 1, 3, "two"},
+				{"testParseValid", 1, 4, "three"},
+				{"testParseValid", 2, 5, "four"},
+				{"testParseValid", 3, 6, "five"},
+				{"testParseValid", 2, 7, "six"},
+				{"testParseValid", 2, 8, "seven"},
 			})
 		})
 		doctype, root, err := parseNodes(protonodes)
@@ -45,28 +45,28 @@ func TestParseProtonodes(t *testing.T) {
 			So(err, ShouldBeNil)
 			So(doctype, ShouldEqual, "html")
 			So(root, ShouldResemble, node{
-				protonode{"", 0, 2, "one"},
+				protonode{"testParseValid", 0, 2, "one"},
 				[]node{
 					node{
-						protonode{"", 1, 3, "two"},
+						protonode{"testParseValid", 1, 3, "two"},
 						nil,
 					},
 					node{
-						protonode{"", 1, 4, "three"},
+						protonode{"testParseValid", 1, 4, "three"},
 						[]node{
 							node{
-								protonode{"", 2, 5, "four"},
+								protonode{"testParseValid", 2, 5, "four"},
 								[]node{node{
-									protonode{"", 3, 6, "five"},
+									protonode{"testParseValid", 3, 6, "five"},
 									nil,
 								}},
 							},
 							node{
-								protonode{"", 2, 7, "six"},
+								protonode{"testParseValid", 2, 7, "six"},
 								nil,
 							},
 							node{
-								protonode{"", 2, 8, "seven"},
+								protonode{"testParseValid", 2, 8, "seven"},
 								nil,
 							},
 						},
@@ -120,17 +120,17 @@ func TestParseProtonodes(t *testing.T) {
 		})
 	})
 	Convey("Invalid input", t, func() {
-		protonodes := parseProtonodes("", testParseInvalid)
+		protonodes := parseProtonodes("testParseInvalid", testParseInvalid)
 		Convey("Should parse into protonodes", func() {
 			So(protonodes, ShouldResemble, []protonode{
-				{"", 0, 1, "doctype html"},
-				{"", 3, 2, "one"},
-				{"", 2, 3, "two"},
-				{"", 1, 4, "three"},
-				{"", 0, 5, "four"},
-				{"", 4, 6, "five"},
-				{"", 1, 7, "six"},
-				{"", 2, 8, "seven"},
+				{"testParseInvalid", 0, 1, "doctype html"},
+				{"testParseInvalid", 3, 2, "one"},
+				{"testParseInvalid", 2, 3, "two"},
+				{"testParseInvalid", 1, 4, "three"},
+				{"testParseInvalid", 0, 5, "four"},
+				{"testParseInvalid", 4, 6, "five"},
+				{"testParseInvalid", 1, 7, "six"},
+				{"testParseInvalid", 2, 8, "seven"},
 			})
 		})
 		_, _, err := parseNodes(protonodes)
