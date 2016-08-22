28d976eb9ca344c960874ec20ad0d49a52c4541e
Nathaniel Ringo <remexre@gmail.com>
2016-08-22 14:59:27 -0500

    All of attribute parsing except for value.

--- parser/node.go
+++ parser/node.go
@@ -49,15 +49,15 @@ func parseNode(src []rune) (node ast.Node, i int, err string) {
 	var parsingContent bool
 	for i < len(src) && !parsingContent {
 		i0 := i
-		var attr ast.Attr
-		attr, parsingContent, i, err = parseAttr(src, i)
+		var attrs []ast.Attr
+		attrs, parsingContent, i, err = parseAttr(src, i)
 		if err != "" {
 			return
 		}
 		if i0 == i {
 			break
 		}
-		element.Attrs = append(element.Attrs, attr)
+		element.Attrs = append(element.Attrs, attrs...)
 	}
 
 	if i < len(src) {
@@ -69,7 +69,7 @@ func parseNode(src []rune) (node ast.Node, i int, err string) {
 		} else if c == '=' {
 			childNode = ast.CodeNode(content)
 		} else {
-			err = fmt.Sprintf("Unrecognized character %#v", string(src[i]))
+			err = fmt.Sprintf("Unexpected character %#v", string(src[i]))
 			return
 		}
 		element.Children = append(element.Children, childNode)
@@ -92,28 +92,90 @@ func parseTag(src []rune, i0 int) (tag string, i int, err string) {
 	return
 }
 
-func parseAttr(src []rune, i0 int) (attr ast.Attr, parsingContent bool, i int, err string) {
+func parseAttr(src []rune, i0 int) (attrs []ast.Attr, parsingContent bool, i int, err string) {
 	i = i0
 	switch src[i0] {
 	case '#':
 		var id string
 		id, i, err = parseTag(src, i0+1)
-		attr = ast.Attr{Name: "id", Value: []ast.Node{ast.TextNode(id)}}
+		attrs = []ast.Attr{ast.Attr{Name: "id", Value: []ast.Node{ast.TextNode(id)}}}
 		return
 	case '.':
 		var class string
 		class, i, err = parseTag(src, i0+1)
-		attr = ast.Attr{Name: "class", Value: []ast.Node{ast.TextNode(class)}}
+		attrs = []ast.Attr{ast.Attr{Name: "class", Value: []ast.Node{ast.TextNode(class)}}}
+		return
+	case '(':
+		attrs, i, err = parseAttrs(src, i0+1)
 		return
 	case '=':
 		parsingContent = true
 		return
 	}
 
-	if unicode.IsSpace(src[i0]) {
+	if unicode.IsSpace(src[i]) {
 		parsingContent = true
 	} else {
-		err = fmt.Sprintf("Unrecognized character %#v", string(src[i0]))
+		err = fmt.Sprintf("Unexpected character %#v", string(src[i]))
+	}
+	return
+}
+
+func parseAttrs(src []rune, i0 int) (attrs []ast.Attr, i int, err string) {
+	i = i0
+	keepGoing := true
+	for keepGoing && i < len(src) {
+		var name string
+		name, i, err = parseAttrName(src, i)
+		if err != "" {
+			return
+		}
+		if src[i] != '=' {
+			err = fmt.Sprintf("Unexpected character %#v in attribute", string(src[i]))
+			return
+		}
+		var value []ast.Node
+		value, i, err = parseAttrValue(src, i)
+		if err != "" {
+			return
+		}
+		attrs = append(attrs, ast.Attr{Name: name, Value: value})
+
+		if src[i] == ',' {
+			i++
+			for unicode.IsSpace(src[i]) {
+				i++
+			}
+		} else if src[i] == ')' {
+			i++
+			keepGoing = false
+		} else {
+			err = fmt.Sprintf("Unexpected character %#v in attribute", string(src[i]))
+			return
+		}
+	}
+	return
+}
+
+func parseAttrName(src []rune, i0 int) (name string, i int, err string) {
+	i = i0
+	l := len(src)
+	for i < l && isTagCharacter(src[i]) {
+		if src[i] == '=' {
+			name = string(src[i0:i])
+			if len(name) == 0 {
+				err = "missing attribute name"
+			}
+			return
+		}
+		i++
+	}
+	err = fmt.Sprintf("Unexpected character %#v in attribute name", string(src[i]))
+	return
+}
+
+func parseAttrValue(src []rune, i0 int) (value []ast.Node, i int, err string) {
+	for i = i0; src[i] != ',' && src[i] != ')'; i++ {
 	}
 	return
 }
